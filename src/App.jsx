c9396c887017f7827cf6f8e5f4164100d2e60fda
minhlong149 <minhlong9514@gmail.com>
2023-04-23 14:11:08 +0700

    Build navigation header

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,46 @@
+import React, { useState } from "react";
+import { Nav, Navbar } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
 function App() {
-  return <div className="App">Hello World</div>;
+  const [user, setUser] = useState(null);
+
+  function login(user = null) {
+    console.log("login");
+    setUser(user);
+  }
+
+  function logout() {
+    console.log("logout");
+    setUser(null);
+  }
+
+  return (
+    <div className="App">
+      <Navbar bg="light" expand="lg">
+        <Navbar.Brand as={Link} to="/">
+          Movie Reviews
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link as={Link} to="/movies">
+              Movies
+            </Nav.Link>
+            {user === null ? (
+              <Nav.Link as={Link} to="/login" onClick={login}>
+                Login
+              </Nav.Link>
+            ) : (
+              <Nav.Link as={Link} to="/" onClick={logout}>
+                Logout
+              </Nav.Link>
+            )}
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    </div>
+  );
 }
 
-export default App
+export default App;
