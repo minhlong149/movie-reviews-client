7f79630e8805bbaa3b9e65b0ea45522beb43b5cd
minhlong149 <minhlong9514@gmail.com>
2023-05-24 22:41:01 +0700

    Add pagination

--- src/components/MoviesList.jsx
+++ src/components/MoviesList.jsx
@@ -19,11 +19,22 @@ function MoviesList() {
   const [searchRating, setSearchRating] = useState("");
   const [ratings, setRatings] = useState(["All Ratings"]);
 
+  const [currentPage, setCurrentPage] = useState(0);
+  const [entriesPerPage, setEntriesPerPage] = useState(0);
+  const [currentSearchMode, setCurrentSearchMode] = useState("");
+
   useEffect(() => {
     console.log("Retrieving movies and ratings");
-    retrieveMovies();
     retrieveRatings();
   }, []);
+
+  useEffect(() => {
+    retrieveNextPage();
+  }, [currentPage]);
+
+  useEffect(() => {
+    setCurrentPage(0);
+  }, [currentSearchMode]);
 
   useEffect(() => {
     console.log("Movies updated", movies);
@@ -31,10 +42,26 @@ function MoviesList() {
 
   const retrieveMovies = async () => {
     try {
-      const response = await MovieDataService.getAll();
+      const response = await MovieDataService.getAll(currentPage);
       setMovies(response.data.movies);
+      setCurrentPage(response.data.page);
+      setEntriesPerPage(response.data.entries_per_page);
+      setCurrentSearchMode("");
     } catch (error) {
       console.log(error);
+    }
+  };
+
+  const retrieveNextPage = () => {
+    switch (currentSearchMode) {
+      case "findByTitle":
+        findByTitle();
+        break;
+      case "findByRating":
+        findByRating();
+        break;
+      default:
+        retrieveMovies();
     }
   };
 
@@ -59,7 +86,7 @@ function MoviesList() {
 
   const find = async (query, by) => {
     try {
-      let response = await MovieDataService.find(query, by);
+      let response = await MovieDataService.find(query, by, currentPage);
       setMovies(response.data.movies);
     } catch (error) {
       console.log(error);
@@ -68,11 +95,13 @@ function MoviesList() {
 
   const findByTitle = () => {
     console.log(`Find movie with title '${searchTitle}' `);
+    setCurrentSearchMode("findByTitle");
     find(searchTitle, "title");
   };
 
   const findByRating = () => {
     console.log(`Find movie with rating '${searchRating}' `);
+    setCurrentSearchMode("findByRating");
     if (searchRating === "All Ratings") {
       retrieveMovies();
     } else {
@@ -119,20 +148,32 @@ function MoviesList() {
             <Card>
               <Card.Img
                 variant="top"
-                src={movie.poster + "/100px180"}
+                src={`${movie.poster}/100px180`}
                 loading="lazy"
               />
               <Card.Body>
                 <Card.Title>{movie.title}</Card.Title>
                 <Card.Text>Rating: {movie.rated}</Card.Text>
                 <Card.Text>{movie.plot}</Card.Text>
-                <Button as={Link} to={"/movies/" + movie._id}>
+                <Button as={Link} to={`/movies/${movie._id}`}>
                   View Reviews
                 </Button>
               </Card.Body>
             </Card>
           </Col>
         ))}
+      </Row>
+
+      <Row className="mt-4 justify-content-center">
+        <Col md={6} className="text-center">
+          Showing page: {+currentPage + 1}.
+          <Button
+            variant="link"
+            onClick={() => setCurrentPage(+currentPage + 1)}
+          >
+            Get next {entriesPerPage} results
+          </Button>
+        </Col>
       </Row>
     </Container>
   );
